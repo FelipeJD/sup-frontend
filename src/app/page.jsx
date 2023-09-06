fc2abd1f02c221134b82121bfd2ef198cc5b99ed
doguebart <douglasweelber.2004@outlook.com>
2023-09-06 17:31:55 -0300

    base project created

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className='w-full h-screen'>
+            <h1 className='text-center py-5'>home</h1>
+        </div>
+    )
+}
+
+export default App;
